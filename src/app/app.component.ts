aab4b81678a5dd4f1fde3027f556bcd9da26bae6
Emersson2000 <62248761+Emersson2000@users.noreply.github.com>
2021-09-12 23:19:20 -0500

    Registro Usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  miFormulario = new FormGroup({
+     nombre : new FormControl('', Validators.required), 
+     apellido : new FormControl('', Validators.required), 
+     fechaNacimiento : new FormControl('', Validators.required), 
+     direccion : new FormControl('', Validators.required)
+  });
 }
